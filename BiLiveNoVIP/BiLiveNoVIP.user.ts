25a56f367514e0ddf2360eea80972ea3b77934f4
lzghzr <me@lzzr.me>
2026-07-14 19:41:32 +0800

    [BiLiveNoVIP] 屏蔽醒目留言头像

--- BiLiveNoVIP/BiLiveNoVIP.user.ts
+++ BiLiveNoVIP/BiLiveNoVIP.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                bilibili直播净化
 // @namespace           https://github.com/lzghzr/GreasemonkeyJS
-// @version             4.3.9
+// @version             4.3.10
 // @author              lzghzr
 // @description         增强直播屏蔽功能, 提高直播观看体验
 // @icon                data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGNpcmNsZSBjeD0iMTYiIGN5PSIxNiIgcj0iMTUiIHN0cm9rZT0iIzAwYWVlYyIgc3Ryb2tlLXdpZHRoPSIyIiBmaWxsPSJub25lIi8+PHRleHQgZm9udC1mYW1pbHk9Ik5vdG8gU2FucyBDSksgU0MiIGZvbnQtc2l6ZT0iMjIiIHg9IjUiIHk9IjIzIiBzdHJva2U9IiMwMDAiIHN0cm9rZS13aWR0aD0iMCIgZmlsbD0iIzAwYWVlYyI+5ruaPC90ZXh0Pjwvc3ZnPg==
@@ -1092,6 +1092,8 @@ $<mut_n>("text",{attrs:{"font-family":"Noto Sans CJK SC","font-size":"14",x:"5",
 /* SuperChat 保留条 */
 #pay-note-panel-vm,
 .chat-item .bottom-background,
+/* SuperChat 聊天条 左上角头像 */
+.chat-item .card-item-middle-top-left,
 /* SuperChat 聊天条 右上角电池 */
 .chat-item .card-item-top-right,
 /* SuperChat 按钮 */
